ba4cee8ba357f82490cfc8475a033e9e23dd99d0
Fernando <38165979+femastro@users.noreply.github.com>
2022-03-30 16:32:13 -0300

    --amend

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,4 @@
 function sendData(){
     var nota = document.getElementById('nota')
-    var formulario = document.getElementById('formulario').value
     nota.innerHTML='<div class="alert alert-success alert-dismissible fade show" role="alert">Iniciando Proceso Espere !!!......</div>'
-    formulario.submit()
-
 }
