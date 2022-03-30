e1bbfddb55ec9c4b24097d0d89afad37f97ceec4
Fernando <38165979+femastro@users.noreply.github.com>
2022-03-30 17:46:07 -0300

    --amend

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
 function sendData(){
-    var nota = document.getElementById('nota')
-    nota.innerHTML='<div class="alert alert-success alert-dismissible fade show" role="alert">Iniciando Proceso Espere !!!......</div>'
+    var nota = document.getElementById('nota');
+    nota.innerHTML='<div class="alert alert-danger alert-dismissible fade show" role="alert">Iniciando Proceso Espere !!!......</div>';
 }
