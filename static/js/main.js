02182d126cd76f1181825eff22d6d76c140b2eea
Fernando <38165979+femastro@users.noreply.github.com>
2022-03-31 18:36:13 -0300

    se agrego spinner para la espera

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
 function sendData(){
-    var nota = document.getElementById('nota');
-    nota.innerHTML='<div class="alert alert-danger alert-dismissible fade show" role="alert">Iniciando Proceso Espere !!!......</div>';
+    var spinner = document.getElementById('spinner');
+    spinner.innerHTML='<div class="alert alert-danger alert-dismissible fade show" role="alert">&nbsp;Iniciando Proceso Espere !!!..... &nbsp;&nbsp;&nbsp;&nbsp;<span class="mt-5"><div class="spinner-border" role="status"></span></div></div>';
 }
